ad47ae5c6f4cf9ebf2ea68883c5ec624c28d2372
SolomDev00 <ew11solom@gmail.com>
2024-09-24 19:42:07 +0300

    F3zkorny Azkar Page is done.

--- src/components/website-navbar/index.tsx
+++ src/components/website-navbar/index.tsx
@@ -5,72 +5,40 @@ import azkarImg from "../../assets/azkar.png";
 import mosqueImg from "../../assets/mosque.png";
 import heartImg from "../../assets/heart.png";
 import personImg from "../../assets/person.png";
-import { Link as ScrollLink, animateScroll as scroll } from "react-scroll";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
-    return (
-        <nav>
-            <div className="container">
-                <img className="logo icon" src={logoImg} alt="logo" />
-                <div className="links">
-                    <ScrollLink
-                        onClick={() => scroll.scrollToTop()}
-                        to="/"
-                        title={"Bringovia SMSID"}
-                        aria-label="home"
-                        className="logo"
-                    >
-                        <img className="icon-sm" src={quranImg} alt="quran icon" />
-                        <div>قرآن كريم</div>
-                    </ScrollLink>
-                    <ScrollLink
-                        className="cursor-pointer"
-                        activeClass="active"
-                        to="azkar"
-                        spy={true}
-                        smooth={true}
-                        offset={-70}
-                        duration={500}
-                    >
-                        <img className="icon-sm" src={azkarImg} alt="azkar icon" />
-                        <div>الآذكار</div>
-                    </ScrollLink>
-                    <ScrollLink
-                        className="cursor-pointer"
-                        activeClass="active"
-                        to="azkar"
-                        spy={true}
-                        smooth={true}
-                        offset={-70}
-                        duration={500}
-                    >
-                        <img className="icon-sm" src={hadeethImg} alt="hadeeth icon" />
-                        <div>الأحاديث</div>
-                    </ScrollLink>
-                    <ScrollLink
-                        className="cursor-pointer"
-                        activeClass="active"
-                        to="azkar"
-                        spy={true}
-                        smooth={true}
-                        offset={-70}
-                        duration={500}
-                    >
-                        <img className="icon-sm" src={mosqueImg} alt="mosque icon" />
-                        <div>الصلاة</div>
-                    </ScrollLink>
+    return <nav>
+        <div className="container">
+            <img className="logo icon" src={logoImg} alt="logo" />
+            <div className="links">
+                <NavLink to="/quran">
+                    <img className="icon-sm" src={quranImg} alt="quran icon" />
+                    <div>قرآن كريم</div>
+                </NavLink>
+                <NavLink to="/azkar">
+                    <img className="icon-sm" src={azkarImg} alt="azkar icon" />
+                    <div>الآذكار</div>
+                </NavLink>
+                <NavLink to="/hadeth">
+                    <img className="icon-sm" src={hadeethImg} alt="hadeeth icon" />
+                    <div>الأحاديث</div>
+                </NavLink>
+                <NavLink to="/sl3a">
+                    <img className="icon-sm" src={mosqueImg} alt="mosque icon" />
+                    <div>الصلاة</div>
+                </NavLink>
+            </div>
+            <div className="left">
+                <div className="fav card-icon-sm">
+                    <img src={heartImg} alt="fav-icon" />
                 </div>
-                <div className="left">
-                    <div className="fav card-icon-sm">
-                        <img src={heartImg} alt="fav-icon" />
-                    </div>
-                    <div className="card-icon-sm profile">
-                        <img src={personImg} alt="profile-img" />
-                    </div>
+                <div className="card-icon-sm profile">
+                    <img src={personImg} alt="profile-img" />
                 </div>
             </div>
-        </nav>
-    );
+        </div>
+    </nav>;
 };
 
 export default Navbar;
