df5b5e4e94030d9351bb1ee7e3f9848d42db1a78
SolomDev00 <ew11solom@gmail.com>
2024-09-27 19:14:42 +0300

    Fix All Errors v3

---
+++ src/components/hadiths/HadithsView.tsx
@@ -0,0 +1,34 @@
+import { useContext } from "react";
+import { HadithsContext } from "../../contexts/hadithsContext";
+import { SoArrowLeft, SoArrowRight } from "solom-icon";
+
+const HadithsView: React.FC = () => {
+  const { currentCategory, currentHadithsItem, navigateHadiths, currentHadithsIndex } = useContext(HadithsContext)!;
+
+  if (!currentCategory || !currentHadithsItem) {
+    return <div>لا يوجد حديث</div>;
+  }
+
+  return (
+    <div className="zekr-view">
+      <h1>{currentCategory.name}</h1>
+      <h2>
+        {currentHadithsIndex + 1}/{currentCategory.data.length}
+      </h2>
+      <div className="zekr-container">
+        <p>{currentHadithsItem.text}</p>
+        <div>{currentHadithsItem.disc}</div>
+      </div>
+      <div className="arrows">
+        <div className={`boxArr bg-[#76c853] rounded-md p-2 ${currentHadithsIndex === currentCategory.data.length - 1 ? "disable" : ""}`} onClick={() => navigateHadiths("next")}>
+          <SoArrowRight />
+        </div>
+        <div className={`boxArr bg-[#76c853] rounded-md p-2 ${currentHadithsIndex === 0 ? "disable" : ""}`} onClick={() => navigateHadiths("prev")}>
+          <SoArrowLeft />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HadithsView;
